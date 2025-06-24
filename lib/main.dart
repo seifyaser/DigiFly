c1f6d6305609a8515879daade82eb56dab69dc28
seifyaser <saifyasser386@gmail.com>
2025-06-24 03:06:34 +0300

    initialize localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:digify/generated/l10n.dart';
 import 'package:digify/presentation/TextEditor_screen/cubit/text_editor_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:digify/routes/app_routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +18,13 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => TextEditorCubit(),
       child: MaterialApp(
+        localizationsDelegates: [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
         debugShowCheckedModeBanner: false,
         routes: AppRoutes.routes,
         initialRoute: AppRoutes.initial,
